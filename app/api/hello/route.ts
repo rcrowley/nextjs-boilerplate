dc196323a6b530b360e00cf533d7dd2e2f1c4795
Richard Crowley <r@rcrowley.org>
2024-04-19 23:13:10 -0700

    JSON response

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,9 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request: Request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+  return new Response(JSON.stringify({
+    "hello": `Hello from ${process.env.VERCEL_REGION}`,
+  }), {headers: {
+    "Content-Type": "application/json; charset=utf8",
+  }});
 }
