bfdc438ef40b58432b27dbc8124abc4e6e0906ef
Richard Crowley <r@rcrowley.org>
2024-04-19 23:14:36 -0700

    Let's see what's going on here

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -3,6 +3,7 @@ export const dynamic = 'force-dynamic'; // static by default, unless reading the
 export function GET(request: Request) {
   return new Response(JSON.stringify({
     "hello": `Hello from ${process.env.VERCEL_REGION}`,
+    "env": process.env,
   }), {headers: {
     "Content-Type": "application/json; charset=utf8",
   }});
